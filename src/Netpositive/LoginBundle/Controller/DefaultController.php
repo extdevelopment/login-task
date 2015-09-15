e563f6b90df66e04cc88d9fc6c09273da1fa55ee
zsolt.kobor <zsolt.kobor@gmail.com>
2015-09-15 18:05:39 +0200

    Display fullName or email, set default_target_path, always_use_default_target_path

--- src/Netpositive/LoginBundle/Controller/DefaultController.php
+++ src/Netpositive/LoginBundle/Controller/DefaultController.php
@@ -3,11 +3,28 @@
 namespace Netpositive\LoginBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Model\UserInterface;
 
+/**
+ * DefaultController.
+ *
+ * @author zsolt.kobor
+ */
 class DefaultController extends Controller
 {
+    /**
+     * @throws AccessDeniedException
+     *
+     * @return Response A Response instance
+     */
     public function indexAction()
     {
-        return $this->render('NetpositiveLoginBundle:Default:index.html.twig', array());
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        return $this->render('NetpositiveLoginBundle:Default:index.html.twig', array('user' => $user));
     }
 }
